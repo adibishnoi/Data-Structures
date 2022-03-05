0bbe597313c50a7158a66b2e6c36ef6b67f495e0
adibishnoi <101010633+adibishnoi@users.noreply.github.com>
2022-03-05 21:49:10 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        int array[] = {1,2,3,4,5};
+        for(int i = 0;i< array.length;i++){
+            System.out.println(array[i]);
+        }
+        System.out.println("original array");
+        for(int i = array.length-1;i>=0;i--){
+            System.out.println(array[i]);
+        }
+        System.out.println("rev array");
+
+        
     }
 }
